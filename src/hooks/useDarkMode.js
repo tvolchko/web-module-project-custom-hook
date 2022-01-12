00c019cce63fe07900adde23941c8ffa66b9c8ff
ted <tvolchko@gmail.com>
2022-01-12 13:17:00 -0600

    mvp complete

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,8 @@
+import {useState} from "react"
+import useLocalStorage from "./useLocalStorage"
+
+const useDarkMode = () => {
+    const [darkMode, setDarkMode] = useLocalStorage('mode', false)
+    return [darkMode, setDarkMode]
+}
+export default useDarkMode
